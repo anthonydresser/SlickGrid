77d04743fd08b664dcdf08b4dc63cec59cb7ae64
Anthony Dresser <andresse@microsoft.com>
2017-08-14 14:00:11 -0700

    fixes around handleSelectedRanges event

--- slick.grid.js
+++ slick.grid.js
@@ -485,7 +485,7 @@ if (typeof Slick === "undefined") {
       selectionModel = model;
       if (selectionModel) {
         selectionModel.init(self);
-        handleSelectedRangesChanged(this, selectionModel.getSelectedRanges);
+        handleSelectedRangesChanged(this.onSelectedRangesChanged, selectionModel.getSelectedRanges);
         selectionModel.onSelectedRangesChanged.subscribe(handleSelectedRangesChanged);
       }
     }
